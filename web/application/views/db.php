6695cbad21b6ca675b28c5eb8e93774e7c5a0514
samu <samu_eyes@yahoo.com>
2016-09-28 10:30:46 +0700

    test

--- web/application/views/db.php
+++ web/application/views/db.php
@@ -73,6 +73,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div id="body"> 
 		Test dulu
 		Belum nemu cara ambil data
+		
+		test2
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
