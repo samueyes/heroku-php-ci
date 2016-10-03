9fff427ae1bec3c4b6fa69337826e915dd474bcc
samu <samu_eyes@yahoo.com>
2016-10-03 15:47:34 +0700

    set local
    ec2-54-235-254-56.compute-1.amazonaws.com conf pg to local

--- web/pgadmin/conf/config.inc.php
+++ web/pgadmin/conf/config.inc.php
@@ -15,7 +15,7 @@
 
 	// Hostname or IP address for server.  Use '' for UNIX domain socket.
 	// use 'localhost' for TCP/IP connection on this computer
-	$conf['servers'][0]['host'] = 'ec2-54-235-254-56.compute-1.amazonaws.com';
+	$conf['servers'][0]['host'] = 'localhost';
 
 	// Database port on server (5432 is the PostgreSQL default)
 	$conf['servers'][0]['port'] = 5432;
